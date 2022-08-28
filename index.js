cd66edf0cc4e606edb149f3918301c25aafd8a88
Evan <47719478+Mynameisevanbro@users.noreply.github.com>
2022-08-28 11:10:59 -0700

    Added animations & buttons (Still does nothing))

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var canvas_height = 50
 // GUI
 function canvas_text(text, pos, size, color, font, alignment="center") {
 	// define canvas
-	const canvas = document.getElementById("gd");
+	const canvas = document.getElementById("title_graphic");
 	const ctx = canvas.getContext("2d");
 	// draw title
 	let text_size = size;
@@ -20,7 +20,7 @@ function canvas_text(text, pos, size, color, font, alignment="center") {
 
 function canvas_rect(rect, color) {
 	// define canvas
-	const canvas = document.getElementById("gd");
+	const canvas = document.getElementById("title_graphic");
 	const ctx = canvas.getContext("2d");
 	// draw title
 	ctx.fillStyle = color;
@@ -31,37 +31,70 @@ function canvas_rect(rect, color) {
 // pages
 function page_title() {
 	// define canvas
-	const canvas = document.getElementById("gd");
+	const canvas = document.getElementById("title_graphic");
 	const ctx = canvas.getContext("2d");
 	// mobile format view
 	if (format == "PC") {
-		// title rectangle
-		let x = canvas_width / 20;
-		let y = canvas_height / 20;
+		// apply canvas dimensions
+		canvas.width = canvas_width - (canvas_width / 20) * 2;
+		canvas.height = canvas_height / 8;
+		// position
+		canvas.style.marginLeft = `${canvas_width / 20}px`;
+		canvas.style.marginTop = `${canvas_height / 40}px`;
+		// render title view
 		let w = canvas_width - (canvas_width / 20) * 3;
 		let h = canvas_height / 8;
-		canvas_rect([x, y, w, h], "rgb(100, 100, 100)")
+		canvas_rect([0, 0, w, h], "rgb(100, 100, 100)");
 		// triangle part of title background
 		ctx.beginPath();
-		ctx.moveTo(x + w, y);
-		ctx.lineTo(x + w + (canvas_width / 20), y);
-		ctx.lineTo(x + w, y + h);
-		ctx.lineTo(x + w - 1, y);
+		ctx.moveTo(w, 0);
+		ctx.lineTo(w + (canvas_width / 20), 0);
+		ctx.lineTo(w, h);
+		ctx.lineTo(w - 1, 0);
 		ctx.closePath();
 		ctx.fill();
 		// draw title
-		x = canvas_width / 18;
-		y = canvas_height / 8;
+		let x = canvas_width / 50;
+		let y = canvas_height / 14;
 		canvas_text("TriFlash", [x, y], canvas_height / 12, "white", "Poppins", "left");
 		// draw subtitle
-		y = canvas_height / 6.5;
+		y = canvas_height / 9;
 		canvas_text(`Online Edition - PC webpage view`, [x, y], canvas_height / 40, "white", "Poppins", "left");
-		// credit
-		x = canvas_width / 2
-		y = canvas_height - (canvas_height / 70)
-		canvas_text("Code & Program by: Mynameisevanbro", [x, y], canvas_height / 50, "white", "Poppins");
+		// title buttons
+		let s = `${canvas_height / 30}px`;
+		x = `${canvas_width / 20}px`
+		w = `${canvas_width / 3}px`;
+		h = `${canvas_height / 10}px`;
+		let d = (canvas_height / 2.5) - (canvas_height / 4)
+		// title start button
+		let button = document.getElementById("title_start");
+		button.style.width = w;
+		button.style.height = h;
+		button.style.marginLeft = x;
+		button.style.fontSize = s;
+		button.style.marginTop = `${canvas_height / 4}px`;
+		// title select button
+		button = document.getElementById("title_select");
+		button.style.width = w;
+		button.style.height = h;
+		button.style.marginLeft = x;
+		button.style.fontSize = s;
+		button.style.marginTop = `${canvas_height / 2.5}px`;
+		// title credit button
+		button = document.getElementById("title_credit");
+		button.style.width = w;
+		button.style.height = h;
+		button.style.marginLeft = x;
+		button.style.fontSize = s;
+		button.style.marginTop = `${(canvas_height / 2.5) + d}px`;
 	}
 	if (format == "mobile") {
+		// apply canvas dimensions
+		canvas.width = canvas_width
+		canvas.height = canvas_height / 5
+		// position
+		canvas.style.marginLeft = "0px"
+		canvas.style.marginTop = "0px"
 		// title rectangle
 		let h = canvas_height / 5
 		canvas_rect([0, 0, canvas_width, h], "rgb(100, 100, 100)")
@@ -75,6 +108,33 @@ function page_title() {
 		// credit
 		y = canvas_height - (canvas_height / 70)
 		canvas_text("Code & Program by: Mynameisevanbro", [x, y], canvas_height / 50, "white", "Poppins")
+		// title buttons
+		let s = `${canvas_height / 30}px`;
+		x = `${canvas_width / 20}px`
+		w = `${canvas_width - (canvas_width / 10)}px`;
+		h = `${canvas_height / 5}px`;
+		let d = (canvas_height / 2) - (canvas_height / 4)
+		// title start button
+		let button = document.getElementById("title_start");
+		button.style.width = w;
+		button.style.height = h;
+		button.style.marginLeft = x;
+		button.style.fontSize = s;
+		button.style.marginTop = `${canvas_height / 4}px`;
+		// title select button
+		button = document.getElementById("title_select");
+		button.style.width = w;
+		button.style.height = h;
+		button.style.marginLeft = x;
+		button.style.fontSize = s;
+		button.style.marginTop = `${canvas_height / 2}px`;
+		// title credit button
+		button = document.getElementById("title_credit");
+		button.style.width = w;
+		button.style.height = h;
+		button.style.marginLeft = x;
+		button.style.fontSize = s;
+		button.style.marginTop = `${(canvas_height / 2) + d}px`;
 	}
 	
 }
@@ -82,23 +142,21 @@ function page_title() {
 
 // start up
 function reset() {
+	console.log("Flashcards program by Mynameisevanbro!")
 	// set title
 	document.getElementById("title").innerHTML = "TriFlash Online Edition"
 	// define canvas
-	const canvas = document.getElementById("gd");
+	const canvas = document.getElementById("title_graphic");
 	const ctx = canvas.getContext("2d");
 	// update canvas dimensions
 	canvas_width = window.innerWidth - 20;
 	canvas_height = window.innerHeight - 25;
 	// set platform format
-	if (canvas_width > 960) {
+	if (screen.width > 960) {
 		format = "PC"
 	} else {
 		format = "mobile"
 	}
-	// apply canvas dimensions
-	canvas.width = canvas_width
-	canvas.height = canvas_height
 	// render title page
 	page_title()
 }
