7a78aea9f7ab2741f5e506d60b6b640da97483bc
Evan <47719478+Mynameisevanbro@users.noreply.github.com>
2022-12-17 21:43:08 -0500

    Triflash

--- set0.js
+++ set0.js
@@ -2,6 +2,13 @@ var set0 = `
 
 
 
+a
+Type "a"
+`
+var set1 = `
+
+
+
 el ananá
 pineapple (masculine)
 
@@ -216,7 +223,7 @@ el lodo
 mud
 `
 
-var set1 = `
+var set2 = `
 
 
 
@@ -567,6 +574,1523 @@ var set3 = `
 
 
 
-a
-Type "a"
+mentir
+to lie
+
+
+
+enfrentar
+to face, to confront
+
+
+
+quedarse
+to stay
+
+
+
+despreciar
+to despise (not menospreciar)
+
+
+
+confiar en
+to trust (in)
+
+
+
+burlarse
+to make fun of
+
+
+
+juzgar
+to judge
+
+
+
+entrentener
+to entertain
+
+
+
+apreciar
+to appreciate
+
+
+
+menospreciar
+to underestimate/to despise (not despreciar)
+
+
+
+perderse
+to get lost
+
+
+
+merecer
+to deserve
+
+
+
+dirigir
+to manage/to direct
+
+
+
+prender
+to turn on
+
+
+
+chismear
+to gossip
+
+
+
+consolar
+to console/to comfort
+
+
+
+gastar
+to spend
+
+
+
+alegrar
+to make happy
+
+
+
+ladrar
+to bark
+
+
+
+invadir
+to invade
+
+
+
+conducir
+to drive
+
+
+
+odiar
+to hate
+
+
+
+asegurar
+to assure
+
+
+
+escoger/elegir
+to choose
+
+
+
+ganar
+to win/to earn
+
+
+
+provacar
+to provoke
+
+
+
+culpar
+to blame/to accuse
+
+
+
+tratar
+to treat
+
+
+
+saltar
+to jump
+
+
+
+agarrar
+to grab/to catch
+
+
+
+advinar
+to guess
+
+
+
+confundir
+to confuse
+
+
+
+vencer
+to defeat/to overcome (not derrotar)
+
+
+
+cobrar
+to charge (money)
+
+
+
+sugerir
+to suggest
+
+
+
+afeirtar
+to shave
+
+
+
+pienar
+to comb
+
+
+
+vestirse
+to get dressed
+
+
+
+sorprender
+to surprise
+
+
+
+suplicar
+to beg
+
+
+
+sobrevivir
+to survive
+
+
+
+apoyar
+to support
+
+
+
+arrancar
+to start (a car/engine)
+
+
+
+bendecir
+to bless
+
+
+
+ubicar
+to locate
+
+
+
+tender
+to hang (not colgar)
+
+
+
+arreglar
+to fix
+
+
+
+despertarse
+to wake up
+
+
+
+propongar
+to propose
+
+
+
+aconsejar
+to advise
+
+
+
+abrochar
+to fasten
+
+
+
+aprobar
+to approve/to pass
+
+
+
+actualizar
+to update
+
+
+
+convertir
+to convert/to turn into/to change
+
+
+
+contribuir
+to contribute
+
+
+
+enriquecer
+to make rich/to enrich
+
+
+
+depender
+to depend
+
+
+
+trascender
+to emerge/to transcend (not surgir)
+
+
+
+manejar
+to handle/to drive (not conducir)
+
+
+
+irritar
+to irritate/to annoy
+
+
+
+surgir
+to arise/to emerge/to appear (not trascender)
+
+
+
+destacar
+to emphasize/to stand out (not sobresalir)
+
+
+
+parquear
+to park (not estacionar)
+
+
+
+chequear
+to check
+
+
+
+salvar
+to save/to rescue
+
+
+
+adquirir
+to acquire/to purchase
+
+
+
+rodear
+to surround/to go around/to encircle
+
+
+
+hallar
+to find/to discover/to locate/to feel/to be apart of
+
+
+
+desembocar
+to flow into
+
+
+
+industrializar
+to industrialize
+
+
+
+cultivar
+to cultivate
+
+
+
+abundar
+to abound (to be abundant)
+
+
+
+extraer
+to extract/to take out
+
+
+
+desarrollar
+to develop
+
+
+
+sobresalir
+to emphasize/to stand out (not destacar)
+
+
+
+florecer
+to flourish
+
+
+
+lograr
+to achieve
+
+
+
+restaurar
+to restore
+
+
+
+abarcar
+to cover (not cubrir)
+
+
+
+contener
+to contain
+
+
+
+obrar
+to act
+
+
+
+poseer
+to possess/to own
+
+
+
+pertenecer
+to pertain/to belong to
+
+
+
+deshacer
+to undo/to unpack
+
+
+
+fundar
+to establish/to build/to found (as in to build)
+
+
+
+emplear
+to use/to employ
+
+
+
+apodar
+to nickname
+
+
+
+rendirse
+to give up/to surrender
+
+
+
+permanecer
+to stay/to remain
+
+
+
+reunir
+to gather
+
+
+
+crear
+to create
+
+
+
+expulsar
+to expel/to throw out/to eject
+
+
+
+partir
+to part/to cut/to split/to set off
+
+
+
+pisar
+to step on/to set foot
+
+
+
+conquistar
+to conquer
+
+
+
+entregar
+to turn in/to deliver/to give
+
+
+
+proteger
+to protect
+
+
+
+derrotar
+to defeat (not vencer)
+
+
+
+suprimir
+to suppress/to abolish/to delete
+
+
+
+estallar
+to explode/to burst
+
+
+
+parlamentar
+to parley (talk with an enemy)
+
+
+
+componer
+to compose/to make up
+
+
+
+engañar
+to cheat/to deceive
+
+
+
+dejar
+to leave (behind) (not salir)
+
+
+
+alabar
+to praise
+
+
+
+exaltar
+to extol/to praise enthusiastically
+
+
+
+encender
+to turn on/to light
+
+
+
+engrasar
+to grease
+
+
+
+hornear
+to bake/to roast
+
+
+
+untar
+to spread
+
+
+
+violentar
+to be violent/to force
+
+
+
+oponer
+to put up/to oppose
+
+
+
+encarcelar
+to imprison
+
+
+
+radicar
+to lie in
+
+
+
+lamentar
+to lament/to regret
+
+
+
+acostumbrar
+to be accustomed to
+
+
+
+acostar
+to put to bed
+
+
+
+consistir en
+to consist (of)
+
+
+
+brindar
+to make a toast (expression of honor or goodwill before drinking)
+
+
+
+venerar
+to venerate/to warship
+
+
+
+exterminar
+to exterminate/to eradicate
+
+
+
+emprender
+to undertake/to start
+
+
+
+ejercitar
+to practice/to exercise
+
+
+
+obligar
+to oblige/to force
+
+
+
+plasmar
+to give expression to
+
+
+
+heredar
+to inherit
+
+
+
+sobrepasar
+to surpass
+
+
+
+moler
+to mill/to grind
+
+
+
+cocer
+to cook (not cocinar)
+
+
+
+reemplazar
+to replace
+
+
+
+madurar
+to mature/to ripen
+
+
+
+mejorar
+to improve
+
+
+
+abrazar
+to hug
+
+
+
+exponer
+to expose/to exhibit/to explain
+
+
+
+acarrear
+to carry
+
+
+
+evitar
+to avoid
+
+
+
+probar
+to taste/to try (not intentar)
+
+
+
+restar
+to subtract/to deduct
+
+
+
+analizar
+to analyze
+
+
+
+roncar
+to snore
+
+
+
+mangar
+to pinch/swipe/nick
+
+
+
+planchar
+to straighten/to iron
+
+
+
+importar
+to matter/to import
+
+
+
+chocar
+to crash
+
+
+
+atiborrar
+to cram/to stuff full
+
+
+
+penar
+to punish
+
+
+
+suspirar
+to sigh
+
+
+
+redactar
+to write (not escribir; to put in writing)
+
+
+
+besar
+to kiss
+
+
+
+manosear
+to handle/to grope
+
+
+
+pegar
+to paste/to glue/to hit
+
+
+
+zambullirse
+to dive/to plunge
+
+
+
+extrañar
+to miss (someone/something)
+
+
+
+bloquear
+to block/to stop
+
+
+
+rescatar
+to rescue
+
+
+
+identificar
+to identify
+
+
+
+intentar
+to try/to attempt (not probar)
+
+
+
+indicar
+to indicate
+
+
+
+rebuscar
+to search carefully (stronger version of buscar)
+
+
+
+contradecir
+to contradict
+
+
+
+decepcionar
+to disappoint
+
+
+
+calificar
+to grade/to describe as/to qualify
+
+
+
+menear
+to shake
+
+
+
+exprimir
+to squeeze
+
+
+
+despedir
+to say goodbye
+
+
+
+revelar
+to reveal/develop
+
+
+
+desfilar
+to parade/to pass by
+
+
+
+murmurar
+to murmur
+
+
+
+aperar
+to harness/to provide/to equip
+
+
+
+entrenar
+to train
+
+
+
+angostar
+to narrow/to make smaller
+
+
+
+desempeñar
+to carry out (to execute)
+
+
+
+averiguar
+to find out
+
+
+
+impresionar
+to impress/to strike
+
+
+
+mudar
+to change/to move
+
+
+
+exagerar
+to exaggerate/to overdo
+
+
+
+tirar
+to throw
+
+
+
+frotar
+to rub/scrub
+
+
+
+disparar
+to shoot/to fire (a weapon)
+
+
+
+aguantar
+to withstand/put up with
+
+
+
+guiñar
+to wink/to blink
+
+
+
+refunfuñar
+to grumble/to complain
+
+
+
+perseguir
+to pursue/to chase
+
+
+
+simbolizar
+to symbolize
+
+
+
+amarrar
+to tie/to fasten
+
+
+
+inventar
+to invent/ to make up (a story)
+
+
+
+cepillar
+to brush/to clean
+
+
+
+atrapar
+to trap/to catch
+
+
+
+puyar
+to prick/to jab
+
+
+
+afligir
+to afflict (harm)/to upset
+
+
+
+enchufar
+to plug in (electricity)/to join
+
+
+
+alzar
+to raise/to lift (not levantar)
+
+
+
+recostar
+to learn/to lay back
+
+
+
+pulir
+to polish
+
+
+
+becar
+to give a scholarship to
+
+
+
+enrizar
+to curl
+
+
+
+inquietar
+to worry
+
+
+
+chapurrear
+to speak only a little
+
+
+
+enfatizar
+to emphasize
+
+
+
+impedir
+to prevent
+
+
+
+consultar
+to consult/to discuss with
+
+
+
+adelantarse
+to go forward/to go ahead/to move ahead/to take the lead
+
+
+
+aislarse
+to isolate oneself
+
+
+
+desgastar
+to wear out, to deplete
+
+
+
+envejecer
+to grow old
+
+
+
+declarar
+to declare/to state
+
+
+
+causar
+to cause
+
+
+
+convencer
+to convince
+
+
+
+equivocarse
+to make a mistake/to be mistaken
+
+
+
+rodar
+to roll
+
+
+
+declamar
+to declamar/to recite
+
+
+
+apagar
+to turn off/to put out (fire)
+
+
+
+describir
+to describe
+
+
+
+cumplir
+to achieve/to fulfill/to carry out
+
+
+
+maximizar
+to maximize
+
+
+
+acceder
+to agree/to access
+
+
+
+aprovechar
+to take advantage of
+
+
+
+esparcir
+to scatter/to spread
+
+
+
+intercambiar
+to exchange
+
+
+
+criar
+to raise
+
+
+
+simplificar
+to simplify
+
+
+
+notar
+to notice
+
+
+
+meter
+to put/to stick/to place
+
+
+
+rogar
+to beg
+
+
+
+encargarse
+to handle/to take care of
+
+
+
+colocar
+to place/to position
+
+
+
+descolgar
+to take down
+
+
+
+aburrir
+to bore
+
+
+
+pescar
+to fish
+
+
+
+realizar
+to perform/to accomplish
+
+
+
+reñir
+to scold
+
+
+
+estornudar
+to sneeze
+
+
+
+admitir
+to admit/to confess
+
+
+
+girar
+to turn/to center on
+
+
+
+difundir
+to spread
+
+
+
+detallar
+to tell in detail
+
+
+
+fresar
+to mill
+
+
+
+abollar
+to dent
+
+
+
+toser
+to cough
+
+
+
+entristecer
+to sadden/to make sad
+
+
+
+echar
+to throw away (propel)
+
+
+
+detener
+to stop/to arrest/to delay
+
+
+
+inclinar
+to incline/to tilt
+
+
+
+mellar
+to notch/to nick/to chip/to scratch
+
+
+
+sujetar
+to hold/to fasten/to secure
+
+
+
+asar
+to grill/to roast
+
+
+
+derramar
+to spill/to shed
+
+
+
+ensabanar
+to wrap up in sheets
+
+
+
+sostener
+to sustain
+
+
+
+barrer
+to sweep
+
+
+
+deslizar
+to slide
+
+
+
+zumbar
+to buzz/to hum/to flutter around
+
+
+
+pizcar
+to pinch/to take in the harvest
+
+
+
+susurrar
+to whisper/to murmur
+
+
+
+apear
+to help down
+
+
+
+tapar
+to cover (with something)/to cover up
+
+
+
+sumir
+to plunge
+
+
+
+sobresaltar
+to startle
+
+
+
+matricular
+to register/to matriculate
+
+
+
+brincar
+to jump
+
+
+
+empacar
+to pack
+
+
+
+sofocar
+to suffocate
+
+
+
+agujerear
+to make holes in
+
+
+
+marear
+to make somebody dizzy or confused
+
+
+
+arrepentirse
+to regret it
+
+
+
+estremecer
+to shake
+
+
+
+aguar
+to water down/to spoil
+
+
+
+contemplar
+to contemplate/to gaze at/to consider
+
+
+
+apuntar
+to aim/to point at/to indicate
+
+
+
+advertir
+to warn
+
+
+
+masticar
+to chew
+
+
+
+empapar
+to soak
+
+
+
+rascar
+to scratch
+
+
+
+alcanzar
+to reach/to attain
+
+
+
+forjarse
+build up/to shape for oneself
+
+
+
+grabar
+to record
+
+
+
+renunciar
+to resign/to relinquish
+
+
+
+recoger
+to pick up/to gather
+
+
+
+disminuir
+to diminish/to decrease
+
+
+
+alejar
+to move away
+`
+var set4 = `
+
+
+
+asustado
+scared/frightened/afraid
+
+
+
+refrescante
+refreshing
+
+
+
+espacioso
+spacious
+
+
+
+común
+common/usual
+
+
+
+acogedor
+cozy/welcoming
+
+
+
+palpable
+palpable/tangible
+
+
+
+extravagante
+extravagant
+
+
+
+agrio
+sour
+
+
+
+chiquitito
+tiny
+
+
+
+doloroso
+painful
+
+
+
+útil
+useful
+
+
+
+natal
+native
+
+
+
+mocho
+blunt (not sharp)
+
+
+
+horro
+sterile/enfranchised
+
+
+
+mecánico
+mechanical
+
+
+
+posterior
+rear/back (position)
+
+
+
+cansancio
+tiredness/fatigue
+
+
+
+indeciso
+indecisive/undecided
+
+
+
+suficiente
+enough/sufficient
+
+
+
+grosero
+rude
+
+
+
+oliente
+smelly
+
+
+
+callado
+silent
+
+
+
+cocido
+cooked
+
+
+
+fornido
+well-built
 `
